c96443d668622aed5966d8f7fac6503b08d57917
Junhong Li <Junhong.Li@scsstudent.org>
2026-01-27 22:31:46 -0800

    feat: test API

---
+++ website/js/script.js
@@ -0,0 +1,21 @@
+const url =
+  "https://student-api-proxy.onrender.com/api/nba-api-free-data.p.rapidapi.com/nba-conference-standings?year=2023";
+const options = {
+  method: "GET",
+  headers: {
+    "X-API-Key":
+      "04fb8593dd38a31178c28b5cce304c2eeae3d7ad33432136839677ba0f7bb390",
+  },
+};
+
+fetch(url, options)
+  .then((response) =>
+    response.json().then((result) => {
+      console.log(result.data); // Your API data
+      console.log(`Cost: $${result.meta.cost}`);
+      console.log(`Remaining: $${result.meta.remaining_budget}`);
+    }),
+  )
+  .catch((error) => {
+    console.log(error);
+  });
