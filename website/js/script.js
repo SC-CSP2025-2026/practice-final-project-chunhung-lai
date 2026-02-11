80cb498f74e8e486d838004535d826ffb0a7093b
Junhong Li <Junhong.Li@scsstudent.org>
2026-02-11 14:47:24 -0800

    debuggginggggg

--- website/js/script.js
+++ website/js/script.js
@@ -1,21 +1,59 @@
-const url =
-  "https://student-api-proxy.onrender.com/api/nba-api-free-data.p.rapidapi.com/ENDPOINT";
+const search_btn = document.querySelector("#search-btn");
+const year_input = document.querySelector("#year-input");
+const search_output = document.querySelector("#search-output");
+
 const options = {
-  method: "GET",
   headers: {
     "X-API-Key":
       "04fb8593dd38a31178c28b5cce304c2eeae3d7ad33432136839677ba0f7bb390",
   },
 };
 
-fetch(url, options)
-  .then((response) =>
-    response.json().then((result) => {
-      console.log(result.data); // Your API data
-      console.log(`Cost: $${result.meta.cost}`);
-      console.log(`Remaining: $${result.meta.remaining_budget}`);
-    }),
-  )
-  .catch((error) => {
-    console.log(error);
-  });
+function renderTeams(teamList, output) {
+  let html = "<ol>";
+
+  for (let i = 0; i < teamList.length; i++) {
+    html += `<li>${teamList[i].team.name}</li>`;
+  }
+
+  html += "</ol>";
+  output.innerHTML = html;
+}
+
+async function loadWesternRanking(year, output) {
+  const URL = `https://student-api-proxy.onrender.com/api/nba-api-free-data.p.rapidapi.com/nba-conference-standings?year=${year}`;
+
+  output.innerHTML = "Loading...";
+  try {
+    const res = await fetch(URL, options);
+    const data = await res.json();
+
+    const standings = data.data.response.standings;
+    const west = standings.find(function (c) {
+      let conferenceName;
+
+      if (c.conference && c.conference.name) {
+        conferenceName = c.conference.name;
+      } else {
+        conferenceName = c.name;
+      }
+
+      return conferenceName === "Western Conference";
+    });
+
+    const westTeams = west.standings.entries;
+    renderTeams(westTeams, output);
+  } catch (err) {
+    output.innerHTML = "Failed to load.";
+  }
+}
+
+search_btn.addEventListener("click", (event) => {
+  const year = year_input.value;
+  if (year && year >= 2000 && year <= 2026) {
+    loadWesternRanking(year, search_output);
+  } else {
+    search_output.innerHTML =
+      "Please enter a valid year between 2000 and 2026.";
+  }
+});
